4c5eee9e942d162ec4a3973a1cd7a2735fbc6595
rakesh <ry251236@gmail.com>
2026-03-25 03:29:01 +0530

    Fixed database config for Render deployment

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -80,16 +80,12 @@ from decouple import config
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', cast=bool)
 
+import dj_database_url
+
 DATABASES = {
-    
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3'
+    )
 }
 
 
